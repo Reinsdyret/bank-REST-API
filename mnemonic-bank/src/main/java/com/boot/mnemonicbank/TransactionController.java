50438d2ff559d486e8b6fe9428bb518c7a21816a
Lars Haukland <lars.haukland@gmail.com>
2023-10-10 18:13:52 +0200

    Transaction controller is up. But not possible to create transactions

--- mnemonic-bank/src/main/java/com/boot/mnemonicbank/TransactionController.java
+++ mnemonic-bank/src/main/java/com/boot/mnemonicbank/TransactionController.java
@@ -27,20 +27,36 @@ public class TransactionController {
 		// Update account balances.
 		long fromAccountID = transaction.getSourceAccountID();
 		long toAccountID = transaction.getDestinationAccountID();
-		Optional<Account> fromAccount = accountRepository.findById(fromAccountID);
-		Optional<Account> toAccount = accountRepository.findById(toAccountID);
+		Optional<Account> fromAccountOptional = accountRepository.findById(fromAccountID);
+		Optional<Account> toAccountOptional = accountRepository.findById(toAccountID);
+		Account fromAccount, toAccount;
+		
+		// Check that accounts exist and return errors if not
+		if(!fromAccountOptional.isPresent()) return badRequestWithMessage("Account with id " + String.valueOf(fromAccountID) + " does not exist");
+		else { // Update account if it exists
+			fromAccount = fromAccountOptional.get();
+		}
+		
+		if(!toAccountOptional.isPresent()) return badRequestWithMessage("Account with id " + String.valueOf(toAccountID) + " does not exist");
+		else { // Update account if it exists
+			toAccount = toAccountOptional.get();
+		}
 		
 		double amount = transaction.getCashAmount();
 		
 		// Check that amount is not more than fromAccount's balance
 		if (amount > fromAccount.getAvailableCash()) {
 			// Return failed response with code 400
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Account to transfer from does not have enough funds");
+			return badRequestWithMessage("Account to transfer from does not have enough funds");
 		}
 		
 		
 		
 		// Return saved transaction
 		return transactionService.saveTransaction(transaction);
+	}
+	
+	private ResponseEntity badRequestWithMessage(String message) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
 	}
 }
